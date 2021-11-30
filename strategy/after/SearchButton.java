ebf8979151e465b97299097a536d363df1f8d989
Jung hongki <82.hongki.jung@gmail.com>
2021-12-01 01:09:31 +0900

    add strategy pattern

---
+++ strategy/after/SearchButton.java
@@ -0,0 +1,20 @@
+package strategy.after;
+
+public class SearchButton {
+
+  private MyProgram myProgram;
+
+  public SearchButton (MyProgram _myProgram) {
+    myProgram = _myProgram;
+  }
+
+  private SearchStrategy searchStrategy = new SearchStratagyAll();
+  
+  public void setSearchStrategy (SearchStrategy _searchStrategy) {
+    searchStrategy = _searchStrategy;
+  }
+
+  public void onClick () {
+    searchStrategy.search();
+  }
+}
